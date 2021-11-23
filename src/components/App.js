368e166e6423c6c5d056c90cec23ff1d9bb9e10e
James Burns <jrburns96@aol.com>
2021-11-23 15:57:53 -0500

    commit tree d310492a752845ab7482a8020162087fa4373f5a

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,25 @@
 import React from "react";
+import NavBar from './NavBar'
+import Home from './Home'
+import About from './About'
+// import username from '../user'
 
+
+// const username = "Liza";
+// const city = "New York";
+
+// const image = "https://i.imgur.com/mV8PQxj.gif";
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home 
+      // username={username}
+      // city={city}
+      />
+      <About 
+      // image={image}
+      />
     </div>
   );
 }
